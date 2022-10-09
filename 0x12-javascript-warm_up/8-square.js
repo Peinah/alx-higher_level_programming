71f8916634b0e73b6c7a8883c4ea6755d877f76e
Peinah <painepeinah@gmail.com>
2022-10-09 09:40:50 -0700

    js warm up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+able File  11 lines (11 sloc)  234 Bytes
+
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
+}
